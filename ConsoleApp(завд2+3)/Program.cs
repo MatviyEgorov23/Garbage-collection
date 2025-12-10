a0e2d7c30cdd2a1abd6ac529ac8c946a98502955
MatviyEgorov23 <149804295+MatviyEgorov23@users.noreply.github.com>
2025-12-10 08:15:43 +0200

    Add files via upload

---
+++ ConsoleApp(завд2+3)/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_завд2_3_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shop shop = new Shop("АТБ", "вул. Центральна 10", ShopType.Продовольчий);
+            shop.ShowInfo();
+
+            shop.Dispose();
+
+            Console.WriteLine("Об'єкт Shop після виклику Dispose().\n");
+        }
+    }
+}
---
+++ ConsoleApp(завд2+3)/Shop.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_завд2_3_
+{
+    enum ShopType
+    {
+        Продовольчий,
+        Господарський,
+        Одяг,
+        Взуття
+    }
+
+    class Shop : IDisposable
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public ShopType Type { get; set; }
+
+        private bool disposed = false;
+
+        public Shop(string name, string address, ShopType type)
+        {
+            Name = name;
+            Address = address;
+            Type = type;
+            Console.WriteLine("Створено об’єкт Shop.");
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Магазин: {Name}");
+            Console.WriteLine($"Адреса: {Address}");
+            Console.WriteLine($"Тип: {Type}");
+        }
+
+        // --- IDisposable ---
+        public void Dispose()
+        {
+            if (!disposed)
+            {
+                Console.WriteLine("Метод Dispose() викликано для Shop.");
+                disposed = true;
+                GC.SuppressFinalize(this);
+            }
+        }
+
+        // --- ДЕСТРУКТОР
+        ~Shop()
+        {
+            Console.WriteLine("Деструктор Shop викликано!");
+        }
+    }
+}
