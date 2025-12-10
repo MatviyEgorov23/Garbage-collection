4cdce22efa30459d61ea03cc1da8701dd041c6f3
MatviyEgorov23 <149804295+MatviyEgorov23@users.noreply.github.com>
2025-12-10 08:13:37 +0200

    Add files via upload

---
+++ ConsoleApp(завд2)/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_завд2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Store s1 = new Store("Shop1", "вул. Центральна, 10", StoreType.Grocery);
+            Store s2 = new Store("Shop2", "вул. Київська, 22", StoreType.Shoes);
+
+            s1.PrintInfo();
+            s2.PrintInfo();
+
+            // Викликаємо Dispose вручну
+            s1.Dispose();
+            s2.Dispose();
+
+            // Для демонстрації фіналізатора
+            s1 = null;
+            s2 = null;
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        }
+    }
+}
---
+++ ConsoleApp(завд2)/Store.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_завд2_
+{
+    public enum StoreType
+    {
+        Grocery,        // продовольчий
+        Household,      // господарський
+        Clothes,        // одяг
+        Shoes           // взуття
+    }
+
+    public class Store : IDisposable
+    {
+        // Поля
+        private string name;
+        private string address;
+        private StoreType type;
+        private bool disposed = false;
+
+        // Властивості
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public string Address
+        {
+            get => address;
+            set => address = value;
+        }
+
+        public StoreType Type
+        {
+            get => type;
+            set => type = value;
+        }
+
+        // Конструктор
+
+        public Store(string name, string address, StoreType type)
+        {
+            Name = name;
+            Address = address;
+            Type = type;
+
+            Console.WriteLine($"Створено магазин: {Name}");
+        }
+
+        // Метод виводу
+
+        public void PrintInfo()
+        {
+            Console.WriteLine("=== Інформація про магазин ===");
+            Console.WriteLine($"Назва: {Name}");
+            Console.WriteLine($"Адреса: {Address}");
+            Console.WriteLine($"Тип: {Type}");
+            Console.WriteLine("==============================");
+        }
+
+        // Реалізація IDisposable
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    // Тут очищаються керовані ресурси
+                    Console.WriteLine($"Dispose: Звільняємо ресурси магазину: {Name}");
+                }
+
+                // Тут очищаються некеровані ресурси (якби були)
+                disposed = true;
+            }
+        }
+
+        // Фіналізатор (деструктор)
+
+        ~Store()
+        {
+            Console.WriteLine($"Фіналізатор: Магазин {Name} видаляється GC.");
+            Dispose(false);
+        }
+    }
+}
