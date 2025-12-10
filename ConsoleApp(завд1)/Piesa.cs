20d3f29337a0da557711c6e4d0c1946f20b32dd2
MatviyEgorov23 <149804295+MatviyEgorov23@users.noreply.github.com>
2025-12-10 08:12:43 +0200

    Add files via upload

---
+++ ConsoleApp(завд1)/Piesa.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_завд1_
+{
+    public class Piesa
+    {
+        private string title;
+        private string author;
+        private string genre;
+        private int year;
+
+        // Властивості
+        public string Title
+        {
+            get => title;
+            set => title = value;
+        }
+
+        public string Author
+        {
+            get => author;
+            set => author = value;
+        }
+
+        public string Genre
+        {
+            get => genre;
+            set => genre = value;
+        }
+
+        public int Year
+        {
+            get => year;
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Рік не може бути від’ємним.");
+                year = value;
+            }
+        }
+
+        // Конструктор
+        public Piesa(string title, string author, string genre, int year)
+        {
+            Title = title;
+            Author = author;
+            Genre = genre;
+            Year = year;
+
+            Console.WriteLine($"Створено п'єсу: {Title}");
+            Console.WriteLine("===========================");
+        }
+
+        // Деструктор
+        ~Piesa()
+        {
+            Console.WriteLine($"Об'єкт п'єси \"{Title}\" знищено.");
+        }
+
+        // Метод виведення
+        public void PrintInfo()
+        {
+            Console.WriteLine($"П'єса: {Title}");
+            Console.WriteLine($"Автор: {Author}");
+            Console.WriteLine($"Жанр: {Genre}");
+            Console.WriteLine($"Рік: {Year}");
+            Console.WriteLine("===========================");
+        }
+    }
+}
---
+++ ConsoleApp(завд1)/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_завд1_
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Piesa p1 = new Piesa("Гамлет", "Вільям Шекспір", "Трагедія", 1600);
+            Piesa p2 = new Piesa("Лісова пісня", "Леся Українка", "Драма-феєрія", 1911);
+
+            p1.PrintInfo();
+            p2.PrintInfo();
+
+            // Примусове видалення (лише для демонстрації)
+            p1 = null;
+            p2 = null;
+
+            // Запускає GC, щоб деструктор викликався
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        }
+    }
+}
