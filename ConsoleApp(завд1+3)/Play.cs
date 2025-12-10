6f195438248483a56d5e87edaf3803247f6f7ca7
MatviyEgorov23 <149804295+MatviyEgorov23@users.noreply.github.com>
2025-12-10 08:14:44 +0200

    Add files via upload

---
+++ ConsoleApp(завд1+3)/Play.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_завд1_3_
+{
+    class Play : IDisposable
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public int Year { get; set; }
+
+        private bool disposed = false;
+
+        public Play(string title, string author, string genre, int year)
+        {
+            Title = title;
+            Author = author;
+            Genre = genre;
+            Year = year;
+            Console.WriteLine("Створено об’єкт Play.");
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"П'єса: {Title}");
+            Console.WriteLine($"Автор: {Author}");
+            Console.WriteLine($"Жанр: {Genre}");
+            Console.WriteLine($"Рік: {Year}");
+        }
+
+        // --- ДЕСТРУКТОР ---
+        ~Play()
+        {
+            Console.WriteLine("Деструктор Play викликано!");
+        }
+
+        // --- IDisposable ---
+        public void Dispose()
+        {
+            if (!disposed)
+            {
+                Console.WriteLine("Метод Dispose() викликано для Play.");
+                disposed = true;
+                GC.SuppressFinalize(this);
+            }
+        }
+    }
+}
---
+++ ConsoleApp(завд1+3)/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_завд1_3_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Play play = new Play("Гамлет", "Вільям Шекспір", "Трагедія", 1601))
+            {
+                play.ShowInfo();
+            }
+
+            Console.WriteLine("Об'єкт Play вийшов з блоку using.\n");
+        }
+    }
+}
